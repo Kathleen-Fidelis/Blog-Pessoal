3e2c9187d6b143e34ad8368ed3132651eb44974e
Kathleen-Fidelis <kathleenfidelis12@gmail.com>
2021-04-29 15:42:40 -0300

    Back-end API Rest - Feito Relacionamento entre as tabelas

--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,56 +6,78 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Entity //informa que esta model é uma entidade e que ela virará uma tabela no banco de dados
-@Table(name = "postagem") //define o nome da tabela no banco
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity // informa que esta model é uma entidade e que ela virará uma tabela no banco de
+		// dados
+@Table(name = "postagem") // define o nome da tabela no banco
 public class Postagem {
 
-	@Id //define o id como uma Primary Key
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //auto-incremento
+	@Id // define o id como uma Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // auto-incremento
 	private long id;
-	
+
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String titulo;
-	
+
 	@NotNull
 	@Size(min = 10, max = 500)
 	private String texto;
-	
-	@Temporal(TemporalType.TIMESTAMP) //define que o conteúdo do atributo data será pego automaticamente do próprio sistema
-	private Date data= new java.sql.Date(System.currentTimeMillis());
-	
-	
+
+	// define que o conteúdo do atributo data será pego automaticamente do próprio sistema
+	@Temporal(TemporalType.TIMESTAMP) 
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
-	public Date getDate() {
+
+	public Date getData() {
 		return data;
 	}
-	public void setDate(Date date) {
-		this.data = date;
+
+	public void setData(Date data) {
+		this.data = data;
 	}
-	
-	
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
 }
