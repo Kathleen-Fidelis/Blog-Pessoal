d98d981ff9ee05290f65c8421edaa846f0bff390
Kathleen-Fidelis <kathleenfidelis12@gmail.com>
2021-05-07 17:22:00 -0300

    Implementação de Security

---
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -0,0 +1,49 @@
+package org.generation.blogPessoal.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
+import org.generation.blogPessoal.model.UserLogin;
+import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+	
+	public Usuario CadastrarUsuario(Usuario usuario){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		String senhaEncoder = encoder.encode(usuario.getSenha()); //codifica a senha e armazena na variável encoder
+		usuario.setSenha(senhaEncoder); //substitui a senha do objeto usuario pela senha criptografada
+		
+		return repository.save(usuario); //salva a senha no banco
+	}
+	
+	public Optional<UserLogin> Logar(Optional<UserLogin> user){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+		
+		if(usuario.isPresent()) {
+			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())) { //compara a senha do usuario com a senha criptografada
+				
+				//concatena usuario e senha para gerar o token criptografado
+				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
+				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII"))); //padrao de criptografia
+				String authHeader = "Basic " + new String(encodedAuth); //adiciona o prefixo Basic ao token
+				
+				user.get().setToken(authHeader);
+				user.get().setNome(usuario.get().getNome());
+				
+				return user;
+			}
+		}
+				return null;
+	}
+}
